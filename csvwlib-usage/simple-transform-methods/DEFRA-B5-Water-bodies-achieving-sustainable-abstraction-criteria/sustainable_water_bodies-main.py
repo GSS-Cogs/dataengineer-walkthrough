cb3e71bb205ed37feb36ff76318ae93e32d62951
Andrew Fergusson <andrew.fergusson@ons.gov.uk>
2021-09-28 13:17:02 +0100

    Initial commit

---
+++ csvwlib-usage/simple-transform-methods/DEFRA-B5-Water-bodies-achieving-sustainable-abstraction-criteria/sustainable_water_bodies-main.py
@@ -0,0 +1,21 @@
+#%%
+import pandas as pd
+from gssutils import *
+
+#%%
+metadata = Scraper(seed='sustainable_water_bodies-info.json')
+metadata.select_dataset(title = lambda x: "B5" in x)
+metadata.dataset.family = 'climate-change'
+
+#%%
+distribution = metadata.distribution(latest=True)
+
+#%%
+df = distribution.as_pandas(encoding='ISO-8859-1')
+
+#%%
+df.to_csv('sustainable_water_bodies-observations.csv', index=False)
+
+#%%
+CatalogMetadata = metadata.as_csvqb_catalog_metadata()
+CatalogMetadata.to_json_file('sustainable_water_bodies-catalog-metadata.json')
